bdf16495db049f3a2ed4d3bf3e063f60d228245b
maparigi <maparigi@student.42.fr>
2023-04-28 19:48:19 +0200

    added blinking text and sender not working

--- .send-text.sh
+++ .send-text.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+#----------colors-----------#
+NC='\033[0m'
+
+Black='\033[0;30m'
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+White='\033[0;37m'
+Blink='\033[5m'
+Bold='\033[1m'
+#---------------------------#
+
+echo -e "${Purple}${Blink}  _____________  ________   __  ______   ________  _______   ________${NC}"
+echo -e "${Blue}${Blink} /_  __/ ____/ |/ /_  __/  /  |/  /   | / ____/ / / /  _/ | / / ____/${NC}"
+echo -e "${Green}${Blink}  / / / __/  |   / / /    / /|_/ / /| |/ /   / /_/ // //  |/ / __/   ${NC}"
+echo -e "${Yellow}${Blink} / / / /___ /   | / /    / /  / / ___ / /___/ __  // // /|  / /___   ${NC}"
+echo -e "${Red}${Blink}/_/ /_____//_/|_|/_/    /_/  /_/_/  |_\____/_/ /_/___/_/ |_/_____/   ${NC}"
+echo ""
+
 echo "Hi! This is an automated text machine!"
+echo "Please enter the sender's name : (or press enter to use default name 'machine')"
+read sender
+if [ -z "$sender" ]; then
+	sender="Machine"
+fi
 echo "Please enter your recipient's phone number:"
 read ph_nb
 echo "Please enter your message:"
@@ -16,6 +43,7 @@ if [ -z "$key" ]; then
 fi
 echo "Sending message to $ph_nb..."
 curl -X POST https://textbelt.com/text \
+     --data-urlencode sender="$sender" \
      --data-urlencode phone="$ph_nb" \
      --data-urlencode message="$msg" \
-     --data-urlencode key="$key"
+     --data-urlencode key="$key" \
